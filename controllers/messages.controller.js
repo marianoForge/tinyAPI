bc3f1c8711d614cd63e24d54cc58887423e38612
Mariano De Simone <marianodesimone@Marianos-Air.fibertel.com.ar>
2022-02-01 12:16:18 -0300

    First Commit

---
+++ controllers/messages.controller.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+function getMessages(req, res) {
+  res.sendFile(
+    path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg')
+  );
+}
+
+function postMessage(req, res) {
+  console.log('Updating Messages');
+}
+
+module.exports = {
+  getMessages,
+  postMessage,
+};
---
+++ models/friends.model.js
@@ -0,0 +1,12 @@
+const friends = [
+  {
+    id: 0,
+    name: 'Lorenzo',
+  },
+  {
+    id: 1,
+    name: 'Mariano',
+  },
+];
+
+module.exports = friends;
